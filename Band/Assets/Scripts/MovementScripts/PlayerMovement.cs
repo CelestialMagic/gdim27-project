2b5cc0a596b36dcc9c5033589b9c1540a6945820
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-05-13 20:05:40 -0700

    Commented Player Movement

--- Band/Assets/Scripts/MovementScripts/PlayerMovement.cs
+++ Band/Assets/Scripts/MovementScripts/PlayerMovement.cs
@@ -7,23 +7,25 @@ using TMPro;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private InputAction upwardMovement;
+    private InputAction upwardMovement;//A set of inputs for moving upward and downward
 
     [SerializeField]
-    private InputAction sideMovement;
+    private InputAction sideMovement;//A set of inputs for moving left and right
 
     [SerializeField]
-    private float moveAmount;
+    private float moveAmount;//The force to move by 
 
     [SerializeField]
-    private TMP_Text myEncounter;
+    private TMP_Text myEncounter;//A deprecated script 
 
     [SerializeField]
-    private TMP_Text myGig;
+    private TMP_Text myGig;//Deprecated
 
     [SerializeField]
-    private EncounterHandler eh;
+    private EncounterHandler eh;//Responsible for hiding and showing encounters found by player
 
+
+    //Enables movement up, down, left, right
     private void OnEnable()
     {
         upwardMovement.Enable();
@@ -36,6 +38,7 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(sideMovement.ReadValue<float>() * Time.deltaTime * moveAmount, upwardMovement.ReadValue<float>() * Time.deltaTime * moveAmount, 0);
     }
 
+    //Handles collisions for encounter and gig squares 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Encounter") 
